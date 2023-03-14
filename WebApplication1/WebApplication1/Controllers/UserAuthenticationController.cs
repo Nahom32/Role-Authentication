daff2ecd8b459c5afbb8b84e7aea44b426bd260e
Nahom32 <senaynahom00@gmail.com>
2023-03-14 10:19:07 +0300

    Added Login controller

---
+++ WebApplication1/WebApplication1/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data.Services;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserAuthenticationController : Controller
+        
+    {
+        private readonly IUserAuthentication _service;
+        public UserAuthenticationController(IUserAuthentication _service)
+        {
+            this._service = _service;  
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _service.LoginAsync(model);
+                if(result.StatusCode == 1)
+                {
+                    return RedirectToAction("Display", "Dashboard");
+                }
+                else
+                {
+                    TempData["msg"] = result.Message;
+                    return RedirectToAction(nameof(Login));
+                }
+
+            }
+            return View(model);
+        }
+        public async Task Logout()
+        {
+            await _service.LogoutAsync();
+        }
+    }
+}
